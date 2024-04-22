a5e93bcb418c1208dfa240fd68f010e317a25f8e
Rachel Silva <rachel.silva@microware.com.br>
2024-04-22 13:30:34 -0300

    Tentando fazer questão 9

---
+++ Exercicios/Aula2/Topico 02.II - Estruturas de Repeticao e String/Exe09/Exe09 resto da divisao/src/App.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Valor de x: ");
+        int x = input.nextInt(); 
+
+        System.out.print("Valor de y: ");
+        int y = input.nextInt(); 
+
+        int aux = 0;
+        int resultado = 0;
+        boolean stop = false;
+        while (stop != true) 
+        {
+            resultado = aux * y; 
+
+            if (resultado == x)
+            {
+                stop = true;
+                System.out.print("A divisão de " + x + " por " + y + " vale " + aux + " e tem resto " + (resultado - x));
+                break;
+            }
+
+            else if (resultado > x)
+            {
+                stop = true;
+                System.out.print("A divisão de " + x + " por " + y + " vale " + (aux-1) + " e tem resto " + ((x/y) - (aux)));
+                break;
+            }
+
+            aux++; 
+        }
+        
+        input.close();
+    }
+}
