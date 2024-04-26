6cb7f8d1c170356d5eb44487b7a227dbc59d9e94
RachelS2 <rachelbs@id.uff.br>
2024-04-26 17:53:15 -0300

    Corrigindo o caso de quando numero é 50

--- Lista1/4. Subprogramacao/Exe02 - numeros romanos/Exe02 - numeros romanos/src/App.java
+++ Lista1/4. Subprogramacao/Exe02 - numeros romanos/Exe02 - numeros romanos/src/App.java
@@ -13,14 +13,17 @@ public class App {
             if (num < 50)
                 System.out.print("O número " + num + " em numerologia romana é: " + zero_A_49[num-1]);
 
-            else if (num >= 50 & num < 100) 
+            else if (num == 50)
+                System.out.print("O número 50 em numerologia romana é: L");
+
+            else if (num > 50 & num < 100) 
             {
                 System.out.print("O número " + num + " em numerologia romana é: L" + zero_A_49[num-51] );
             }
 
             else if (num >= 100 & num < 500) 
             {
-                
+
             }
 
         }
