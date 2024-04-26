d9a61ea475edecc41acf201437de7f14ae2ea60c
Rachel Silva <rachel.silva@microware.com.br>
2024-04-26 08:32:44 -0300

    Finalizando exercicio 3

--- Exercicios/Aula3/Topico 03.I - Variaveis Compostas/Exe03 - multiplicando matrizes/Exe03_/src/App.java
+++ Exercicios/Aula3/Topico 03.I - Variaveis Compostas/Exe03 - multiplicando matrizes/Exe03_/src/App.java
@@ -6,7 +6,7 @@ public class App {
         int[][] matriz_Um = new int[2][3];
         int[][] matriz_Dois = new int[3][2];
 
-        int[][] matriz_Result = new int[3][3];
+        int[][] matriz_Result = new int[2][2];
 
         //MATRIZ UM 
         System.out.print("\n~~~ MATRIZ UM ~~~ \n");
@@ -26,17 +26,18 @@ public class App {
             for(int coluna = 0; coluna < 2; coluna++)
                 {
                     System.out.print("Insira o elemento [" + (linha+1) + ", " + (coluna+1) + "]: ");
-                    matriz_Um[linha][coluna] = input.nextInt();
+                    matriz_Dois[linha][coluna] = input.nextInt();
                 }
         }
 
         //MATRIZ RESULTADO
-        int k = 0;
-        for(int linha = 0; linha < 3; linha++) 
+        for(int linha = 0; linha < 2; linha++) 
         {
-            for(int coluna = 0; coluna < 3; coluna++)
+            int k = 0;
+            for(int coluna = 0; coluna < 2; coluna++)
             {    
                 matriz_Result[linha][coluna] = matriz_Um[linha][k] * matriz_Dois[k][coluna];
+                System.out.print(" \n " + matriz_Result[linha][coluna]);
                 k++;
             }
         } 
