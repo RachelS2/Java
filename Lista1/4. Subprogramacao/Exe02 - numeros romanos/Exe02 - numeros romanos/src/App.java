075528fc32a90d48891889c6b48e8ae897177276
RachelS2 <rachelbs@id.uff.br>
2024-04-26 17:58:16 -0300

    Limitando numero > 0

--- Lista1/4. Subprogramacao/Exe02 - numeros romanos/Exe02 - numeros romanos/src/App.java
+++ Lista1/4. Subprogramacao/Exe02 - numeros romanos/Exe02 - numeros romanos/src/App.java
@@ -10,23 +10,24 @@ public class App {
             System.out.print("O número deve ter até 3 dígitos. Reinicie o programa e tente novamente.");
         
         else {
-            if (num < 50)
+            
+            if (num > 0 & num < 50)
                 System.out.print("O número " + num + " em numerologia romana é: " + zero_A_49[num-1]);
-
+            
             else if (num == 50)
                 System.out.print("O número 50 em numerologia romana é: L");
-
+            
             else if (num > 50 & num < 100) 
             {
                 System.out.print("O número " + num + " em numerologia romana é: L" + zero_A_49[num-51] );
             }
-
+            
             else if (num >= 100 & num < 500) 
             {
-
+                
             }
-
-        }
+        }   
+        
         input.close();
     }
 }
