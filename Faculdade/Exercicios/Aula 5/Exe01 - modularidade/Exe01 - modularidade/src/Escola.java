413cf9be36d9d26bbe1f874e5bc9f92620203e8a
RachelS2 <rachel.silva@microware.com.br>
2024-06-06 13:32:45 -0300

    Começando a fazer o exercicio (modularidade)

--- Faculdade/Exercicios/Aula 5/Exe01 - modularidade/Exe01 - modularidade/src/Escola.java
+++ Faculdade/Exercicios/Aula 5/Exe01 - modularidade/Exe01 - modularidade/src/Escola.java
@@ -7,12 +7,32 @@ public class Escola {
         Professor profChris = new Professor("Christiano", 2);
         Professor profElaine = new Professor("Elaine", 3);
 
-        List<Disciplina> disciplinas = Arrays.asList(new Disciplina("Programação Orientada a Objetos", profLuiz), 
-        new Disciplina("Ética e Informação", profChris), new Disciplina("Inteligência de Negócios", profElaine));
+        List<Disciplina> disciplinas = Arrays.asList
+                                    (new Disciplina("Programação Orientada a Objetos", profLuiz, 10), 
+                                    new Disciplina("Ética e Informação", profChris, 9), 
+                                    new Disciplina("Inteligência de Negócios", profElaine, 8));
         
-        List<Aluno> notaAluna = Arrays.asList(new Aluno("Rachel", 10, disciplinas.get(0)), 
-        new Aluno("Rachel", 9, disciplinas.get(1)), new Aluno("Rachel", 8, disciplinas.get(2)));
+        List<Aluno> notaAluna = Arrays.asList
+                                (new Aluno("Rachel", disciplinas.get(0)), 
+                                new Aluno("Rachel", disciplinas.get(1)), 
+                                new Aluno("Rachel", disciplinas.get(2)));
 
+        ExibeHistorico(notaAluna);
+        ExibeCR(notaAluna);
+    }
+
+    public static void ExibeHistorico(List<Aluno> notaAluno)
+    {
+        System.out.println("~~~~~~~~~~~ HISTÓRICO ~~~~~~~~~~~");
+        System.out.println("Estudante: " + notaAluno.get(0).pegaNome() + "\n");
+        for (Aluno aluno : notaAluno)
+            aluno.ExibeHistorico(aluno);
+    }
+
+    public static void ExibeCR(List<Aluno> notaAluno)
+    {
+        Aluno aluno = notaAluno.get(0);
+        aluno.ExibeCR(notaAluno);
     }
 }
 
@@ -29,24 +49,47 @@ class Disciplina
 {
     String NomeDisciplina; 
     Professor Professor;
-    Disciplina(String nomeDisciplina, Professor professor){
+    Double Nota;
+    Disciplina(String nomeDisciplina, Professor professor, double nota){
         NomeDisciplina = nomeDisciplina;
         Professor = professor;
+        Nota = nota;
     }  
+    public double pegaNota(){
+        return Nota;
+    }
 
+    
 }
 
 class Aluno {
     String Nome;
-    Double Nota;
     Disciplina Disciplina;
-    Aluno(String nome, double nota, Disciplina disciplina){
+    Aluno(String nome, Disciplina disciplina){
         Nome = nome;
-        Nota = nota;
         Disciplina = disciplina;
     }
 
-    public double pegaNota(){
-        return Nota;
+    public String pegaNome(){
+        return Nome;
+    }
+
+    public void ExibeHistorico(Aluno aluno){
+        
+        System.out.println("Disciplina: " + aluno.Disciplina.NomeDisciplina);
+        System.out.println("Nota: " + aluno.Disciplina.Nota);
+        System.out.println(" ");
+    }
+
+    public void ExibeCR(List<Aluno> notaAluno){
+        int soma = 0;
+        System.out.println("~~~~~~~~~~~ COEF. DE REND. ~~~~~~~~~~~");
+        System.out.println("Estudante: " + notaAluno.get(0).Nome);
+        for (int i = 0; i < notaAluno.size(); i++){
+            Aluno aluna = notaAluno.get(i);
+            Disciplina disciplina = aluna.Disciplina;
+            soma += disciplina.Nota; 
+        }
+        System.out.println("Coeficiente de Rendimento: " + soma/3 + "\n");
     }
 }
